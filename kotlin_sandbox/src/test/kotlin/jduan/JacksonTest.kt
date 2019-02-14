049915f3ae0dba61b82f3ad289ddddd1457678ba
Jingjing Duan <duanjingjing@gmail.com>
2019-02-14 13:19:36 -0800

    test jackson json/yaml parser

---
+++ kotlin_sandbox/src/test/kotlin/jduan/JacksonTest.kt
@@ -0,0 +1,19 @@
+package jduan
+
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import java.net.URLDecoder
+
+class JacksonTest {
+    @Test fun `hello world`() {
+        val yamlObjectMapper = ObjectMapper(YAMLFactory()).registerKotlinModule()
+        val str = yamlObjectMapper.writeValueAsString(setOf("hello world", "airbnb rocks"))
+        val test = "com.airbnb.verity.specs.PermissionControlEdgesDAOSpec#the+dao+returns+a+list+of+edges+by+given+user+id"
+        val decodedName = URLDecoder.decode(test, "UTF-8")
+        println("decodedName: $decodedName")
+        assertEquals("Hello, world!", "Hello, world!")
+    }
+}
