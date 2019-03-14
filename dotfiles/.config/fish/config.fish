c0e651af96c09285e7a1191d1754c6b990b7061c
Jingjing Duan <duanjingjing@gmail.com>
2019-03-13 21:57:38 -0700

    set env vars for fzf

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -85,3 +85,9 @@ status --is-interactive; and . (rbenv init -|psub)
 # set up pyenv
 status --is-interactive; and . (pyenv init -|psub)
 
+# fzf
+# Setting fd as the default source for fzf
+export FZF_DEFAULT_COMMAND='fd --type f'
+
+# To apply the command to CTRL-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
