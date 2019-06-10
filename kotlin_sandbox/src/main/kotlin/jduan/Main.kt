9de59413ce37d4fd8409e7cde7c615b1f445f336
Jingjing Duan <duanjingjing@gmail.com>
2019-06-10 11:52:28 -0700

    kotlin: multiline strings

--- kotlin_sandbox/src/main/kotlin/jduan/Main.kt
+++ kotlin_sandbox/src/main/kotlin/jduan/Main.kt
@@ -1,12 +1,18 @@
 package jduan.main
 
-import java.io.File
-
-fun main(args: Array<String>) {
-    val file = File("/tmp/100")
-    file.bufferedReader().use {
-        println(it.readText())
-    }
-
-    println("bye!")
+fun main() {
+  val tests = listOf(
+    "hello",
+    "world"
+  )
+  val str = """
+        You're being notified because it seems like you are the owner of one or more tests
+        that have been disabled. They aren't being run on CI because of that.
+        You can see more info at http://arborist-web.d.musta.ch/find_tests.
+        Please fix them at your earliest convenience and re-enable them. See
+        https://air/unit-test-policy instructions. If you have any questions,
+        feel free to reach out to #build-infra on Slack. Here's the list of tests:
+        $tests
+        """.trimIndent()
+  println(str)
 }
