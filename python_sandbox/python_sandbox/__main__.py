7a6111672aec35c21b37d93184b04babd99baf62
Jingjing Duan <duanjingjing@gmail.com>
2019-07-02 22:00:10 -0700

    document how __name__ works

--- python_sandbox/python_sandbox/__main__.py
+++ python_sandbox/python_sandbox/__main__.py
@@ -1,4 +1,13 @@
 from python_sandbox import app
+# from . import app
+# from .app import run
+# from python_sandbox.app import run
 
+
+# __name__ is a special string attribute of every Python module.
+# Its value would be "python_sandbox.utils.say_hello" in
+# "python_sandbox/utils/say_hello.py".
+# When a module is run directly via python -m some_module, that module is
+# assigned a special value of __name__ == '__main__'
 if __name__ == '__main__':
     app.run()
--- python_sandbox/python_sandbox/utils/say_hello.py
+++ python_sandbox/python_sandbox/utils/say_hello.py
@@ -1,2 +1,3 @@
+print("module name is %s" % __name__)
 def hello():
     print("Hello, world!")
