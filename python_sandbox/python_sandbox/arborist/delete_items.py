e8526a6bd5b144bf362b2e6bf88d831b30950237
Jingjing Duan <duanjingjing@gmail.com>
2019-07-05 11:23:58 -0700

    remove bad items from redis

---
+++ python_sandbox/python_sandbox/arborist/delete_items.py
@@ -0,0 +1,30 @@
+import json
+import redis
+
+rds = redis.Redis(host="localhost", port=6379, db=0)
+bad_items = set()
+redis_key = "resque:queue:notifications"
+pr_numbers = {
+    564695,
+    590740,
+    1153425,
+    614328,
+    1065882,
+    1153128,
+    1158572,
+    1130644,
+}
+start = 0
+end = rds.llen(redis_key)
+
+items = rds.lrange(redis_key, start, end)
+for item in items:
+    event = json.loads(item)
+    if (event['class'] == 'GitHub::Jobs::CreateSubscribedIssueEvents'
+            and event['args'][0] in pr_numbers
+            and event['meta']['context']['job'] == "GitHub::jobs::RetryJobs"):
+        bad_items.add(item)
+
+for bad_item in sorted(bad_items):
+    print("bad item: %s" % bad_item)
+    # rds.lrem(redis_key, 1, bad_item)
