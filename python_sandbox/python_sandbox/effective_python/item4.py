a301a1dbc7b198d539718022441ac56f377e342e
Jingjing Duan <duanjingjing@gmail.com>
2019-07-05 19:24:28 -0700

    effective python: item 4

---
+++ python_sandbox/python_sandbox/effective_python/item4.py
@@ -0,0 +1,26 @@
+from urllib.parse import parse_qs
+
+
+def get_first_int(values, key, default=0):
+    found = values.get(key, [''])
+    if found[0]:
+        found = int(found[0])
+    else:
+        found = default
+    return found
+
+
+def main():
+    my_values = parse_qs('red=5&blue=0&green=', keep_blank_values=True)
+    print("my_values: %s" % my_values)
+    red = get_first_int(my_values, 'red')
+    green = get_first_int(my_values, 'green')
+    opacity = get_first_int(my_values, 'opacity')
+
+    print('Red: %r' % red)
+    print('Green: %r' % green)
+    print('Opacity: %r' % opacity)
+
+
+if __name__ == '__main__':
+    main()
