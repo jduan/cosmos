9849189f8bd2d4b232b8830483a34dd671428842
Jingjing Duan <duanjingjing@gmail.com>
2019-10-03 22:57:23 -0700

    python: play with hydra-core

---
+++ python_sandbox/python_sandbox/hydra/hydra_example.py
@@ -0,0 +1,18 @@
+import hydra
+
+
+# strict mode is on by default
+# You can access keys that don't exist. You can't add keys that aren't in the config file.
+@hydra.main(config_path='config.yaml', strict=True)
+def my_app(cfg):
+    print("Hydra config:")
+    print(cfg.pretty())
+    print()
+    print("driver: %s" % cfg.db.driver)
+    print("pass: %s" % cfg.db.password)
+
+
+# You can also pass config via the command line: (they override the config file)
+# python hydra_example.py db.driver=mysql db.user=omry db.pass=secret
+if __name__ == '__main__':
+    my_app()
