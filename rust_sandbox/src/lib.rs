7dea2dec3d65f064bdc852656644c7773bffc958
Jingjing Duan <duanjingjing@gmail.com>
2019-10-28 22:43:38 -0700

    how to write tests

---
+++ rust_sandbox/src/lib.rs
@@ -0,0 +1,55 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+fn greeting(name: &str) -> String {
+    format!("Hello {}!", "world")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail!");
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle { width: 8, height: 7 };
+        let smaller = Rectangle { width: 5, height: 1 };
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`", result
+        );
+    }
+}
