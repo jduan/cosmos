8a7e35a7f8ff80e9454f4360514de9fd3d18d96e
Jingjing Duan <duanjingjing@gmail.com>
2019-10-29 21:49:36 -0700

    how to write tests

--- rust_sandbox/src/lib.rs
+++ rust_sandbox/src/lib.rs
@@ -15,7 +15,15 @@ fn add_two(a: i32) -> i32 {
 }
 
 fn greeting(name: &str) -> String {
-    format!("Hello {}!", "world")
+    format!("Hello {}!", name)
+}
+
+fn might_panic(value: i32) -> i32 {
+    if value < 1 || value > 100 {
+        panic!("input value must be between 1 and 100");
+    } else {
+        value + 100
+    }
 }
 
 #[cfg(test)]
@@ -34,8 +42,14 @@ mod tests {
 
     #[test]
     fn larger_can_hold_smaller() {
-        let larger = Rectangle { width: 8, height: 7 };
-        let smaller = Rectangle { width: 5, height: 1 };
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
         assert!(larger.can_hold(&smaller));
     }
 
@@ -49,7 +63,38 @@ mod tests {
         let result = greeting("Carol");
         assert!(
             result.contains("Carol"),
-            "Greeting did not contain name, value was `{}`", result
+            "Greeting did not contain name, value was `{}`",
+            result
         );
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        might_panic(102);
+    }
+
+    #[test]
+    #[should_panic(expected = "input value must be between 1 and 100")]
+    fn less_than_1() {
+        might_panic(0);
+    }
+
+    #[test]
+    fn less_than_100() {
+        assert_eq!(199, might_panic(99), "should be equal to 199");
+    }
+
+    // You can make tests return Result<T, E>
+    // This enables you to use the ? operator in the body of tests, which can be
+    // a convenient way to write tests taht should fail if any operation within
+    // them returns an "Err" variant.
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if add_two(3) == 6 {
+            Ok(())
+        } else {
+            Err(String::from("two plus three doesn't equal to five!"))
+        }
     }
 }
