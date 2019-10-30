dc630e6cdcd3fe4ba85e3de3483ae6305187e82d
Jingjing Duan <duanjingjing@gmail.com>
2019-10-29 22:40:02 -0700

    read file

---
+++ rust_minigrep/src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    read_file(filename);
+}
+
+fn read_file(filename: &str) -> String {
+    let contents: String =
+        fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+
+    contents
+}
