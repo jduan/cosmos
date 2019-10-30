605f8ac400add938384f8de4c3d8f1e9caec2684
Jingjing Duan <duanjingjing@gmail.com>
2019-10-29 23:12:43 -0700

    refactor code into src/lib.rs

--- rust_minigrep/src/main.rs
+++ rust_minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use rust_minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,34 +14,8 @@ fn main() {
 
     // similar to unwrap_or_else but since we don't care about the return value (which would be ())
     // in the Ok case, we use "let Err(e)" to handle the error case only.
-    if let Err(e) = run(config) {
+    if let Err(e) = rust_minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-// () means we are returning Unit, ie this function is for side effects only
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents: String = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
 }
