6da90a30cf8126ff565dd91d06f8df3d1a068532
Jingjing Duan <duanjingjing@gmail.com>
2019-10-29 22:47:57 -0700

    refactor main.rs

--- rust_minigrep/src/main.rs
+++ rust_minigrep/src/main.rs
@@ -1,21 +1,47 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    read_file(filename);
+    // similar to unwrap_or_else but since we don't care about the return value (which would be ())
+    // in the Ok case, we use "let Err(e)" to handle the error case only.
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
-fn read_file(filename: &str) -> String {
-    let contents: String =
-        fs::read_to_string(filename).expect("Something went wrong reading the file");
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+// () means we are returning Unit, ie this function is for side effects only
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents: String = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
 
-    contents
+    Ok(())
 }
