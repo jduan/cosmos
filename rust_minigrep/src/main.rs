799741fe47f8f44a3fc82eadfd5d89911fe25b49
Jingjing Duan <duanjingjing@gmail.com>
2019-10-31 22:42:14 -0700

    use eprintln! to write to stderr

--- rust_minigrep/src/main.rs
+++ rust_minigrep/src/main.rs
@@ -6,14 +6,14 @@ use rust_minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     // similar to unwrap_or_else but since we don't care about the return value (which would be ())
     // in the Ok case, we use "let Err(e)" to handle the error case only.
     if let Err(e) = rust_minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
