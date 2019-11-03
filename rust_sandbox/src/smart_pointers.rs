bdf7b23993915b69ee8a718dca3732050c0e37ea
Jingjing Duan <duanjingjing@gmail.com>
2019-11-02 18:46:08 -0700

    Drop trait

--- rust_sandbox/src/smart_pointers.rs
+++ rust_sandbox/src/smart_pointers.rs
@@ -9,6 +9,8 @@ pub fn run() {
     dereference_operator();
     use_mybox();
     deref_coercion();
+    drop_trait();
+    drop_early();
 }
 
 fn box_simple_example() {
@@ -112,4 +114,52 @@ fn deref_coercion() {
     // types and use Deref::deref as many times as necessary to get a reference to match the
     // parameter’s type. The number of times that Deref::deref needs to be inserted is resolved at
     // compile time, so there is no runtime penalty for taking advantage of deref coercion!
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+// This trait lets you customize what happens when a value is about to go out of scope.
+// Usually things like open files and network connections are handled there.
+//
+// In some languages, the programmer must call code to free memory or resources every time they
+// finish using an instance of a smart pointer. If they forget, the system might become overloaded
+// and crash. In Rust, you can specify that a particular bit of code be run whenever a value goes
+// out of scope, and the compiler will insert this code automatically. As a result, you don’t need
+// to be careful about placing cleanup code everywhere in a program that an instance of a
+// particular type is finished with—you still won’t leak resources!
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`", self.data);
+    }
+}
+
+fn drop_trait() {
+    // Variables are dropped in the reverse order of their creation, so 'd' gets dropped before
+    // 'c'.
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointer created.");
+}
+
+fn drop_early() {
+    let c = CustomSmartPointer {
+        data: String::from("some data"),
+    };
+    println!("CustomSmartPointer created.");
+    // Rust doesn't let us call `drop` explicitly because Rust would still automatically call
+    // `drop` at the end of the function. That would be a "double free" error.
+    // c.drop();
+
+    // Instead, you can call the "drop" function from "std::mem::drop" part of prelude, to force
+    // Rust to drop a value before the end of its scope. One example is when using smart pointers
+    // to manage "locks". You might want to release the lock so other code in the same scope can
+    // acquire the lock.
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of the function.");
 }
