486e02db02085c352e7c1812819df1529aade12f
Jingjing Duan <duanjingjing@gmail.com>
2019-11-09 14:24:05 -0800

    add a default handler

--- rust_webserver/src/main.rs
+++ rust_webserver/src/main.rs
@@ -1,3 +1,32 @@
+extern crate iron;
+// this anotation alerts Rust that we plan to use macros exported by the mime crate
+#[macro_use]
+extern crate mime;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Serving on http://localhost:3000...");
+    Iron::new(get_form).http("localhost:3000").unwrap();
+}
+
+fn get_form(_request: &mut Request) -> IronResult<Response> {
+    let mut response = Response::new();
+
+    // The set_mut method uses its argument’s type to decide which part of the response to set
+    response.set_mut(status::Ok);
+    response.set_mut(mime!(Text/Html; Charset=Utf8));
+    response.set_mut(
+        r#"
+    <title>GCD Calculator</title>
+    <form action="/gcd" method="post">
+        <input type="text" name="n">
+        <input type="text" name="n">
+        <button type="submit">Compute GCD</button>
+    </form>
+    "#,
+    );
+
+    Ok(response)
 }
