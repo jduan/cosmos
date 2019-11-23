6f9861526dbfb98049e32d58a18f937059fc095d
Jingjing Duan <duanjingjing@gmail.com>
2019-11-22 23:43:31 -0800

    use failure and exitfailure to handle errors

--- rust_cli/src/main.rs
+++ rust_cli/src/main.rs
@@ -1,3 +1,5 @@
+use exitfailure::ExitFailure;
+use failure::ResultExt;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -11,14 +13,17 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), ExitFailure> {
     let args = Cli::from_args();
 
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|_| format!("could not read file {:?}", &args.path))?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+
+    Ok(())
 }
