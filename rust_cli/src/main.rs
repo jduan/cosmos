49a03f0a04d09cdad1a09ddab0b16235f8c985e9
Jingjing Duan <duanjingjing@gmail.com>
2019-11-23 00:00:44 -0800

    make the code testable

--- rust_cli/src/main.rs
+++ rust_cli/src/main.rs
@@ -13,17 +13,33 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line).expect("Failed to write matched line!");
+        }
+    }
+}
+
 fn main() -> Result<(), ExitFailure> {
     let args = Cli::from_args();
 
     let content = std::fs::read_to_string(&args.path)
         .with_context(|_| format!("could not read file {:?}", &args.path))?;
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_match() {
+        let mut result = Vec::new();
+        find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+        assert_eq!(result, b"lorem ipsum\n");
+    }
 }
