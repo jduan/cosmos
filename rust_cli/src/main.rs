180fffe8ecc8af5789d512276cf3dd819ee76c94
Jingjing Duan <duanjingjing@gmail.com>
2019-11-23 10:29:00 -0800

    move things to a library file

--- rust_cli/src/main.rs
+++ rust_cli/src/main.rs
@@ -1,6 +1,7 @@
 use exitfailure::ExitFailure;
 use failure::ResultExt;
 use structopt::StructOpt;
+mod find_match;
 
 #[derive(StructOpt)]
 struct Cli {
@@ -13,33 +14,13 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            writeln!(writer, "{}", line).expect("Failed to write matched line!");
-        }
-    }
-}
-
 fn main() -> Result<(), ExitFailure> {
     let args = Cli::from_args();
 
     let content = std::fs::read_to_string(&args.path)
         .with_context(|_| format!("could not read file {:?}", &args.path))?;
 
-    find_matches(&content, &args.pattern, &mut std::io::stdout());
+    find_match::find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_find_match() {
-        let mut result = Vec::new();
-        find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
-        assert_eq!(result, b"lorem ipsum\n");
-    }
 }
