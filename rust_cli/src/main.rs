88c0a5ae91cd3d2a86e6622189686cd63a0c364c
Jingjing Duan <duanjingjing@gmail.com>
2019-11-22 23:32:42 -0800

    CLI app is working!

--- rust_cli/src/main.rs
+++ rust_cli/src/main.rs
@@ -1,3 +1,24 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    // The pattern to look for
+    #[structopt(short = "p", long = "pattern")]
+    pattern: String,
+    // The path to the file to read
+    #[structopt(parse(from_os_str))]
+    #[structopt(short = "f", long = "file")]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
