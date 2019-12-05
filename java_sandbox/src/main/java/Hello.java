fefe83f4e38b4bc09664315a1741fc4016d669d0
Jingjing Duan <duanjingjing@gmail.com>
2019-12-05 08:47:57 -0800

    show Java locks are reentrant

--- java_sandbox/src/main/java/Hello.java
+++ java_sandbox/src/main/java/Hello.java
@@ -1,5 +1,31 @@
 public class Hello {
+    private Object lockObject = new Object();
+    public String getName() {
+        synchronized (lockObject) {
+            String firstName = getFirstName();
+            String lastName = getLastName();
+            return firstName + " " + lastName;
+        }
+    }
+
+    private String getLastName() {
+        synchronized (lockObject) {
+            return "John";
+        }
+    }
+
+    private String getFirstName() {
+        synchronized (lockObject) {
+            return "Davis";
+        }
+    }
+
     public String hello() {
         return "world";
+    }
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        System.out.println(hello.getName());
     }
 }
