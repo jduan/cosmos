c3f6d1be8c1445136987829d375cdcf452465b84
Jingjing Duan <duanjingjing@gmail.com>
2019-12-11 22:49:22 -0800

    play with locks in kotlin

--- kotlin_sandbox/src/main/kotlin/jduan/Lock.kt
+++ kotlin_sandbox/src/main/kotlin/jduan/Lock.kt
@@ -1,9 +1,12 @@
 package jduan
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 fun main() {
     val lock = Object()
 
-    // there's no deadlock
+    // there's no deadlock, which shows locks in Java are re-entrant
     synchronized(lock) {
         println("I'm in the synchronized block")
         synchronized(lock) {
@@ -12,4 +15,27 @@ fun main() {
     }
 
     println("done")
+
+    val gitRepo = GitRepo()
+    val job1 = GlobalScope.launch {
+        gitRepo.read()
+    }
+    val job2 = GlobalScope.launch {
+        gitRepo.read()
+    }
+
+    Thread.sleep(10_000)
+    println("Main thread is done")
+}
+
+class GitRepo {
+    private val lock = Object()
+    fun read() {
+        synchronized(lock) {
+            println("Lock acquired")
+            println("thread: ${Thread.currentThread().id}, name: ${Thread.currentThread().name}")
+            Thread.sleep(3000)
+            println("Lock released")
+        }
+    }
 }
