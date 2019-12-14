cd96987a5874a0a71e6612530891ffa67d1d18a1
Jingjing Duan <duanjingjing@gmail.com>
2019-12-14 11:15:28 -0800

    cargo fmt

--- rust_by_example/src/print_things.rs
+++ rust_by_example/src/print_things.rs
@@ -19,20 +19,25 @@ pub fn print_things() {
     println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
 
     // As can named arguments.
-    println!("{subject} {verb} {object}",
-             object="the lazy dog",
-             subject="the quick brown fox",
-             verb="jumps over");
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
 
     // Special formatting can be specified after a `:`.
-    println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
+    println!(
+        "{} of {:b} people know binary, the other half doesn't",
+        1, 2
+    );
 
     // You can right-align text with a specified width. This will output
     // "     1". 5 white spaces and a "1".
-    println!("{number:>width$}", number=1, width=6);
+    println!("{number:>width$}", number = 1, width = 6);
 
     // You can pad numbers with extra zeroes. This will output "000001".
-    println!("{number:>0width$}", number=1, width=6);
+    println!("{number:>0width$}", number = 1, width = 6);
 
     // Rust even checks to make sure the correct number of arguments are
     // used.
