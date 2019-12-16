3f04a662baf6dd36df3fca83609514f2f8a33bf3
Jingjing Duan <duanjingjing@gmail.com>
2019-12-15 19:11:44 -0800

    pattern matching

--- rust_sandbox/src/pattern_matching.rs
+++ rust_sandbox/src/pattern_matching.rs
@@ -36,3 +36,26 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
     }
 }
+
+fn get_number_value(n: u32) -> &'static str {
+    match n {
+        1 => "One",
+        2 | 3 | 5 | 7 | 11 => "Prime",
+        13...19 => "Teen",
+        _ => "Aren't special",
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::pattern_matching::get_number_value;
+
+    #[test]
+    fn test_get_number_value() {
+        assert_eq!("One", get_number_value(1));
+        assert_eq!("Prime", get_number_value(3));
+        assert_eq!("Teen", get_number_value(13));
+        assert_eq!("Teen", get_number_value(19));
+        assert_eq!("Aren't special", get_number_value(190));
+    }
+}
