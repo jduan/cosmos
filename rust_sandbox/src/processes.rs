e264f08c698cd635f0867bde907aaef21b079e97
Jingjing Duan <duanjingjing@gmail.com>
2019-12-27 13:27:39 -0800

    interact with subprocess via pipes

--- rust_sandbox/src/processes.rs
+++ rust_sandbox/src/processes.rs
@@ -1,6 +1,10 @@
 #[cfg(test)]
 mod tests {
-    use std::process::Command;
+    use std::error::Error;
+    use std::io::{Read, Write};
+    use std::process::{Command, Stdio};
+
+    static PANGRAM: &'static str = "the quick brown fox jumped over the lazy dog\n";
 
     #[test]
     fn get_output_of_external_process_success() {
@@ -24,5 +28,39 @@ mod tests {
         assert!(!output.status.success());
         let s = String::from_utf8_lossy(&output.stderr);
         println!("rustc failed and stderr was:\n{}", s);
+    }
+
+    #[test]
+    /// The "std::Child" struct represents a running child process, and exposes the "stdin",
+    /// "stdout", and "stdout" handles for interaction with the underlying process via pipes.
+    fn interact_with_process_via_pipes() {
+        let process = match Command::new("wc")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+        {
+            Err(why) => panic!("couldn't spawn wc: {}", why.description()),
+            Ok(proc) => proc,
+        };
+
+        match process.stdin.unwrap().write_all(PANGRAM.as_bytes()) {
+            Err(why) => panic!("couldn't write to wc stdin: {}", why.description()),
+            Ok(_) => println!("sent pangram to wc"),
+        }
+
+        let mut s = String::new();
+        match process.stdout.unwrap().read_to_string(&mut s) {
+            Err(why) => panic!("couldn't read wc stdout: {}", why.description()),
+            Ok(_) => {
+                println!("wc responded with:\n{}", s);
+                let counts: Vec<u32> = s
+                    .trim()
+                    .split_whitespace()
+                    .into_iter()
+                    .map(|count| count.parse::<u32>().unwrap())
+                    .collect();
+                assert_eq!(vec![1, 9, 45], counts);
+            }
+        }
     }
 }
