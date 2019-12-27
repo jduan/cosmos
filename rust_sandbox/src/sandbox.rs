9293a6feceabdaaad422763ec57e16f02db774df
Jingjing Duan <duanjingjing@gmail.com>
2019-12-27 12:18:45 -0800

    Rust mutability

--- rust_sandbox/src/sandbox.rs
+++ rust_sandbox/src/sandbox.rs
@@ -35,6 +35,27 @@ impl Person {
     }
 }
 
+pub fn two_words() -> (String, String) {
+    (format!("fellow"), format!("Rustaceans"))
+}
+
+/// Concatenate `suffix` onto the end of `prefix`.
+pub fn join_words(prefix: &mut String, suffix: &str) {
+    prefix.push(' '); // separate the words with a space
+    prefix.push_str(suffix);
+}
+
+// Challenge: Convert `join_words` to use borrowing, not ownership.
+// The new function should mutate `prefix` in place, and should not
+// take ownership of `suffix`.
+//
+// Hint: If you'd like a hint as to how to proceed, open
+// <http://intorust.com/hint/mutable_borrow_1/>.
+
+// Question: Now that you've converted `join_words`, what happens if you
+// call `join_words` using the same string for `prefix` and `suffix`?
+// Why?
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -46,5 +67,23 @@ mod tests {
         let name = jill.name();
         let first_name = name.first_name();
         assert_eq!("Jill", first_name);
+    }
+
+    #[test]
+    fn test_mutability() {
+        let mut buffer = String::from("Rustacean");
+        for i in 0..buffer.len() {
+            let slice = &buffer[i..];
+            println!("slice: {:?}", slice);
+        }
+
+        buffer.push_str("s");
+        println!("buffer itself: {:?}", buffer);
+    }
+    #[test]
+    fn test_mutability2() {
+        let (mut str1, str2) = two_words();
+        join_words(&mut str1, &str2);
+        assert_eq!("fellow Rustaceans", str1);
     }
 }
