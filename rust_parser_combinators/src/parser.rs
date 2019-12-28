ce5d2f68aa50f52f28245e7e078a6e90f47b3251
Jingjing Duan <duanjingjing@gmail.com>
2019-12-27 22:58:29 -0800

    parse an identifier

--- rust_parser_combinators/src/parser.rs
+++ rust_parser_combinators/src/parser.rs
@@ -9,6 +9,30 @@ fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<InputOutput,
     }
 }
 
+/// Parse the next identifier.
+/// An identifier starts with one alphabetical character and is followed by zero or more
+/// of either an alphabetical character, a number, or a dash.
+fn identifier(input: &str) -> Result<(&str, String), &str> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+
+    while let Some(next) = chars.next() {
+        if next.is_alphanumeric() || next == '-' {
+            matched.push(next);
+        } else {
+            break;
+        }
+    }
+
+    let next_index = matched.len();
+    Ok((&input[next_index..], matched))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -20,5 +44,21 @@ mod tests {
         assert_eq!(Ok(("", ())), parse_joe("Hello Joe!"));
         assert_eq!(Ok((" Hello Robert!", ())), parse_joe("Hello Joe! Hello Robert!"));
         assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
+    }
+
+    #[test]
+    fn test_identifier() {
+        assert_eq!(
+            Ok(("", "i-am-an-identifier".to_string())),
+            identifier("i-am-an-identifier")
+        );
+        assert_eq!(
+            Ok((" entirely an identifier", "not".to_string())),
+            identifier("not entirely an identifier")
+        );
+        assert_eq!(
+            Err("!not at all an identifier"),
+            identifier("!not at all an identifier")
+        );
     }
 }
