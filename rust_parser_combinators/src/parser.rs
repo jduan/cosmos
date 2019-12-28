550b380d1ddad7674e26f24ec5be422275a77ea4
Jingjing Duan <duanjingjing@gmail.com>
2019-12-27 23:11:12 -0800

    parser combinator

--- rust_parser_combinators/src/parser.rs
+++ rust_parser_combinators/src/parser.rs
@@ -1,8 +1,14 @@
-type InputOutput<'a> = (&'a str, ());
+/// We're going to write a parser for a simplified version of XML. It looks like this:
+///
+/// ```
+/// <parent-element>
+///   <single-element attribute="value" />
+/// </parent-element>
+/// ```
 
 /// This function returns a parser function that matches the input string with
 /// an expected string.
-fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<InputOutput, &str> {
+fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
     move |input| match input.get(0..expected.len()) {
         Some(next) if next == expected => Ok((&input[expected.len()..], ())),
         _ => Err(input),
@@ -33,16 +39,44 @@ fn identifier(input: &str) -> Result<(&str, String), &str> {
     Ok((&input[next_index..], matched))
 }
 
+/// This is a parser combiner. It combines two parsers, P1 and P2, and return another parser
+/// that applies both P1 and P2 and returns a pair of (R1, R2).
+fn pair<P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
+where
+    P1: Fn(&str) -> Result<(&str, R1), &str>,
+    P2: Fn(&str) -> Result<(&str, R2), &str>,
+{
+    move |input| match parser1(input) {
+        Ok((next_input, result1)) => match parser2(next_input) {
+            Ok((final_input, result2)) => Ok((final_input, (result1, result2))),
+            Err(err) => Err(err),
+        },
+        Err(err) => Err(err),
+    }
+}
+
+/// This is a combinator that changes the type of the result of a parser by applying another function.
+fn map<P, F, A, B>(parser: P, map_fn: F) -> impl Fn(&str) -> Result<(&str, B), &str>
+where
+    P: Fn(&str) -> Result<(&str, A), &str>,
+    F: Fn(A) -> B,
+{
+    move |input| parser(input).map(|(next_input, result)| (next_input, map_fn(result)))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use pretty_assertions::{assert_eq, assert_ne};
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn test_match_literal() {
         let parse_joe = match_literal("Hello Joe!");
         assert_eq!(Ok(("", ())), parse_joe("Hello Joe!"));
-        assert_eq!(Ok((" Hello Robert!", ())), parse_joe("Hello Joe! Hello Robert!"));
+        assert_eq!(
+            Ok((" Hello Robert!", ())),
+            parse_joe("Hello Joe! Hello Robert!")
+        );
         assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
     }
 
@@ -59,6 +93,26 @@ mod tests {
         assert_eq!(
             Err("!not at all an identifier"),
             identifier("!not at all an identifier")
+        );
+    }
+
+    #[test]
+    fn test_pair() {
+        let tag_opener = pair(match_literal("<"), identifier);
+        assert_eq!(
+            Ok(("/>", ((), "my-first-element".to_string()))),
+            tag_opener("<my-first-element/>")
+        );
+        assert_eq!(Err("oops"), tag_opener("oops"));
+        assert_eq!(Err("!oops"), tag_opener("<!oops"));
+    }
+
+    #[test]
+    fn test_map() {
+        let tag_opener = map(pair(match_literal("<"), identifier), |(_left, right)| right);
+        assert_eq!(
+            Ok(("/>", "my-first-element".to_string())),
+            tag_opener("<my-first-element/>")
         );
     }
 }
