ebb975f6f5b49f75c918706a21867427a43aae15
Jingjing Duan <duanjingjing@gmail.com>
2019-12-29 22:16:57 -0800

    parse a lot more things

--- write_an_interpreter/src/lexer.rs
+++ write_an_interpreter/src/lexer.rs
@@ -1,9 +1,10 @@
+use std::iter::Peekable;
 use std::str::Chars;
 
 pub struct Lexer<'a> {
     //    position: usize,      // current position in input
     //    read_position: usize, // next position in input, used for peaking at the next char
-    iter: Chars<'a>,
+    iter: Peekable<Chars<'a>>,
 }
 
 impl<'a> Lexer<'a> {
@@ -11,11 +12,13 @@ impl<'a> Lexer<'a> {
         Self {
             //            position: 0,
             //            read_position: 1,
-            iter: input.chars(),
+            iter: input.chars().peekable(),
         }
     }
 
     pub fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+
         if let Some(ch) = self.iter.next() {
             match ch {
                 ',' => Token::Delimiter(Delimiter::Comma),
@@ -26,8 +29,21 @@ impl<'a> Lexer<'a> {
                 ';' => Token::Delimiter(Delimiter::Semicolon),
                 '=' => Token::Operator(Operator::Assignment),
                 '+' => Token::Operator(Operator::PlusSign),
+                '-' => Token::Operator(Operator::MinusSign),
+                '!' => Token::Operator(Operator::Bang),
+                '*' => Token::Operator(Operator::Asterisk),
+                '/' => Token::Operator(Operator::Slash),
+                '<' => Token::Operator(Operator::LessThan),
+                '>' => Token::Operator(Operator::GreaterThan),
                 // TODO: handle other kinds of tokens, such as identifiers, keywords etc.
-                n if n.is_alphabetic() => Token::Identifier(self.read_identifier(ch)),
+                n if n.is_alphabetic() => {
+                    let identifier = self.read_identifier(ch);
+                    match Keyword::convert(&identifier) {
+                        Some(keybword) => Token::Keyword(keybword),
+                        None => Token::Identifier(identifier),
+                    }
+                }
+                n if n.is_numeric() => Token::Literal(self.read_integer(ch)),
                 _ => Token::SpecialToken(SpecialToken::Illegal),
             }
         } else {
@@ -37,9 +53,10 @@ impl<'a> Lexer<'a> {
 
     fn read_identifier(&mut self, ch: char) -> Identifier {
         let mut buffer = vec![ch];
-        while let Some(ch) = self.iter.next() {
-            if Self::is_letter(ch) {
-                buffer.push(ch);
+        while let Some(ch) = self.iter.peek() {
+            if Self::is_letter(*ch) {
+                buffer.push(*ch);
+                self.iter.next();
             } else {
                 break;
             }
@@ -48,8 +65,33 @@ impl<'a> Lexer<'a> {
         buffer.into_iter().collect()
     }
 
+    fn read_integer(&mut self, ch: char) -> Literal {
+        let mut buffer = vec![ch];
+        while let Some(ch) = self.iter.peek() {
+            if ch.is_numeric() {
+                buffer.push(*ch);
+                self.iter.next();
+            } else {
+                break;
+            }
+        }
+
+        let s: String = buffer.into_iter().collect();
+        Literal::Integer(s.parse::<i32>().unwrap())
+    }
+
     fn is_letter(ch: char) -> bool {
         'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+    }
+
+    fn skip_whitespace(&mut self) {
+        while let Some(ch) = self.iter.peek() {
+            if ch.is_whitespace() {
+                self.iter.next();
+            } else {
+                break;
+            }
+        }
     }
 }
 
@@ -60,6 +102,7 @@ pub enum Token {
     Literal(Literal),
     Operator(Operator),
     Delimiter(Delimiter),
+    Keyword(Keyword),
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -79,6 +122,12 @@ pub enum Literal {
 pub enum Operator {
     Assignment,
     PlusSign,
+    MinusSign,
+    Bang,
+    Asterisk,
+    Slash,
+    LessThan,
+    GreaterThan,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -91,9 +140,30 @@ pub enum Delimiter {
     Semicolon,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum Keyword {
     Function,
     Let,
+    If,
+    Else,
+    Return,
+    True,
+    False,
+}
+
+impl Keyword {
+    pub fn convert(identifier: &str) -> Option<Self> {
+        match identifier {
+            "fn" => Some(Keyword::Function),
+            "let" => Some(Keyword::Let),
+            "if" => Some(Keyword::If),
+            "else" => Some(Keyword::Else),
+            "return" => Some(Keyword::Return),
+            "true" => Some(Keyword::True),
+            "false" => Some(Keyword::False),
+            _ => None,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -119,8 +189,113 @@ mod tests {
     }
 
     #[test]
+    fn full_test() {
+        let input = r"
+let five = 5;
+let ten = 10;
+   let add = fn(x, y) {
+     x + y;
+};
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+
+if (5 < 10) {
+        return true;
+} else {
+        return false;
+}
+
+10 == 10;
+10 != 9;
+        ";
+        let mut lexer = Lexer::new(input);
+
+        let expected_tokens = vec![
+            Token::Keyword(Keyword::Let),
+            Token::Identifier(String::from("five")),
+            Token::Operator(Operator::Assignment),
+            Token::Literal(Literal::Integer(5)),
+            Token::Delimiter(Delimiter::Semicolon),
+            Token::Keyword(Keyword::Let),
+            Token::Identifier(String::from("ten")),
+            Token::Operator(Operator::Assignment),
+            Token::Literal(Literal::Integer(10)),
+            Token::Delimiter(Delimiter::Semicolon),
+            Token::Keyword(Keyword::Let),
+            Token::Identifier(String::from("add")),
+            Token::Operator(Operator::Assignment),
+            Token::Keyword(Keyword::Function),
+            Token::Delimiter(Delimiter::LeftParen),
+            Token::Identifier(String::from("x")),
+            Token::Delimiter(Delimiter::Comma),
+            Token::Identifier(String::from("y")),
+            Token::Delimiter(Delimiter::RightParen),
+            Token::Delimiter(Delimiter::LeftBrace),
+            Token::Identifier(String::from("x")),
+            Token::Operator(Operator::PlusSign),
+            Token::Identifier(String::from("y")),
+            Token::Delimiter(Delimiter::Semicolon),
+            Token::Delimiter(Delimiter::RightBrace),
+            Token::Delimiter(Delimiter::Semicolon),
+            Token::Keyword(Keyword::Let),
+            Token::Identifier(String::from("result")),
+            Token::Operator(Operator::Assignment),
+            Token::Identifier(String::from("add")),
+            Token::Delimiter(Delimiter::LeftParen),
+            Token::Identifier(String::from("five")),
+            Token::Delimiter(Delimiter::Comma),
+            Token::Identifier(String::from("ten")),
+            Token::Delimiter(Delimiter::RightParen),
+            Token::Delimiter(Delimiter::Semicolon),
+            Token::Operator(Operator::Bang),
+            Token::Operator(Operator::MinusSign),
+            Token::Operator(Operator::Slash),
+            Token::Operator(Operator::Asterisk),
+            Token::Literal(Literal::Integer(5)),
+            Token::Delimiter(Delimiter::Semicolon),
+            Token::Literal(Literal::Integer(5)),
+            Token::Operator(Operator::LessThan),
+            Token::Literal(Literal::Integer(10)),
+            Token::Operator(Operator::GreaterThan),
+            Token::Literal(Literal::Integer(5)),
+            Token::Delimiter(Delimiter::Semicolon),
+            Token::Keyword(Keyword::If),
+            Token::Delimiter(Delimiter::LeftParen),
+            Token::Literal(Literal::Integer(5)),
+            Token::Operator(Operator::LessThan),
+            Token::Literal(Literal::Integer(10)),
+            Token::Delimiter(Delimiter::RightParen),
+            Token::Delimiter(Delimiter::LeftBrace),
+            Token::Keyword(Keyword::Return),
+            Token::Keyword(Keyword::True),
+            Token::Delimiter(Delimiter::Semicolon),
+            Token::Delimiter(Delimiter::RightBrace),
+            Token::Keyword(Keyword::Else),
+            Token::Delimiter(Delimiter::LeftBrace),
+            Token::Keyword(Keyword::Return),
+            Token::Keyword(Keyword::False),
+            Token::Delimiter(Delimiter::Semicolon),
+            Token::Delimiter(Delimiter::RightBrace),
+            Token::Literal(Literal::Integer(10)),
+        ];
+
+        for expected_token in expected_tokens {
+            assert_eq!(expected_token, lexer.next_token());
+        }
+    }
+
+    #[test]
     fn test_read_identifier() {
         let mut lexer = Lexer::new("hello world");
         assert_eq!(Token::Identifier(String::from("hello")), lexer.next_token());
+    }
+
+    #[test]
+    fn test_hello() {
+        let mut iter = "hello".chars().peekable();
+        assert_eq!('h', iter.next().unwrap());
+        assert_eq!('e', iter.next().unwrap());
+        assert_eq!(10, "10".parse::<i32>().unwrap());
     }
 }
