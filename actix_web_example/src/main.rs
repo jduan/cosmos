deccf3721f3a7cf6df2ffa7418b9baf17f46bf5b
Jingjing Duan <duanjingjing@gmail.com>
2020-01-25 14:59:37 -0800

    actix_web more example

--- actix_web_example/src/main.rs
+++ actix_web_example/src/main.rs
@@ -1,7 +1,25 @@
 use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
+
+struct AppStateWithCounter {
+    counter: Mutex<i32>,
+}
+
+async fn _index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+
+    format!("Request number: {}", counter)
+}
+
+async fn index(data: web::Data<AppState>) -> impl Responder {
+    let app_name = &data.app_name;
+    HttpResponse::Ok().body(format!("Hello {}!", app_name))
 }
 
 async fn index2() -> impl Responder {
@@ -15,11 +33,19 @@ async fn index3() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .route("/", web::get().to(index))
+            .app_data(counter.clone())
+            // Scope acts as a namespace for all routes
+            .service(web::scope("/app").route("/index.html", web::get().to(index)))
+            .route("/", web::get().to(_index))
             .route("/again", web::get().to(index2))
             .service(index3)
+            .service(web::scope("/"))
     })
     .bind("127.0.0.1:8088")?
     .run()
