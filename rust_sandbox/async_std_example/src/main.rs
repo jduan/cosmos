d421cd4a9cb56b3acca9aa2a71f3f19ba33d622d
Jingjing Duan <duanjingjing@gmail.com>
2020-03-03 22:34:09 -0800

    async version

--- rust_sandbox/async_std_example/src/main.rs
+++ rust_sandbox/async_std_example/src/main.rs
@@ -1,24 +1,41 @@
-use std::thread::{sleep, spawn};
+use async_std::task::{sleep, spawn};
 use std::time::Duration;
 
-fn main() {
-    let sleepus = spawn(sleepus);
-    let interruptus = spawn(interruptus);
-
-    sleepus.join().unwrap();
-    interruptus.join().unwrap();
+#[async_std::main]
+async fn main() {
+    // Here we spawn 3 async functions. This shows that all of them run in
+    // the same main thread. You can adjust the number of sleepus we spawn here
+    // and confirm that the number of threads won't change.
+    let sleepus1 = spawn(sleepus());
+    let sleepus2 = spawn(sleepus());
+    let sleepus3 = spawn(sleepus());
+    interruptus().await;
+    sleepus1.await;
+    sleepus2.await;
+    sleepus3.await;
 }
 
-fn sleepus() {
+async fn sleepus() {
     for i in 1..=10 {
         println!("Sleepus {}", i);
-        sleep(Duration::from_millis(500));
+        sleep(Duration::from_millis(500)).await;
     }
 }
 
-fn interruptus() {
+// This is identical to the sleepus() function above!
+#[allow(dead_code)]
+fn sleepus2() -> impl std::future::Future<Output = ()> {
+    async {
+        for i in 1..=10 {
+            println!("Sleepus {}", i);
+            sleep(Duration::from_millis(500)).await;
+        }
+    }
+}
+
+async fn interruptus() {
     for i in 1..=5 {
         println!("Interruptus {}", i);
-        sleep(Duration::from_millis(1000));
+        sleep(Duration::from_millis(1000)).await;
     }
 }
