bcf6a50875a5752988f540c12218e880ca02355c
Jingjing Duan <duanjingjing@gmail.com>
2020-03-03 22:21:14 -0800

    sync version

--- rust_sandbox/async_std_example/src/main.rs
+++ rust_sandbox/async_std_example/src/main.rs
@@ -1,3 +1,24 @@
+use std::thread::{sleep, spawn};
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let sleepus = spawn(sleepus);
+    let interruptus = spawn(interruptus);
+
+    sleepus.join().unwrap();
+    interruptus.join().unwrap();
+}
+
+fn sleepus() {
+    for i in 1..=10 {
+        println!("Sleepus {}", i);
+        sleep(Duration::from_millis(500));
+    }
+}
+
+fn interruptus() {
+    for i in 1..=5 {
+        println!("Interruptus {}", i);
+        sleep(Duration::from_millis(1000));
+    }
 }
