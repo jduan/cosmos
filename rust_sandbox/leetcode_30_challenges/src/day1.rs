1f168d0b3a489aa1755f34c8e8ea4e6ff0057499
Jingjing Duan <duanjingjing@gmail.com>
2020-04-03 22:57:00 -0700

    leetcode: add problem statement

--- rust_sandbox/leetcode_30_challenges/src/day1.rs
+++ rust_sandbox/leetcode_30_challenges/src/day1.rs
@@ -1,3 +1,21 @@
+/// Single Number
+///
+/// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+///
+/// Note:
+///
+/// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+///
+/// Example 1:
+///
+/// Input: [2,2,1]
+/// Output: 1
+///
+/// Example 2:
+///
+/// Input: [4,1,2,1,2]
+/// Output: 4
+
 #[allow(dead_code)]
 pub fn find_single_number(nums: Vec<i32>) -> i32 {
     nums.iter().fold(0, |a, b| a ^ *b)
