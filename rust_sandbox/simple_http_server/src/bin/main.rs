72f0b327fe12c9e52cae064b263130d84987591a
Jingjing Duan <duanjingjing@gmail.com>
2020-04-26 21:39:43 -0700

    graceful shutdown

--- rust_sandbox/simple_http_server/src/bin/main.rs
+++ rust_sandbox/simple_http_server/src/bin/main.rs
@@ -8,12 +8,15 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    // We accept only two requests so we can see how the ThreadPool shuts down gracefully.
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 #[allow(clippy::unused_io_amount)]
--- rust_sandbox/simple_http_server/src/lib.rs
+++ rust_sandbox/simple_http_server/src/lib.rs
@@ -1,15 +1,24 @@
+use crate::Message::NewJob;
+use crate::Message::Terminate;
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 
 /// A job is an arbitrary function.
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+/// We need the ability to send a Terminate message so workers can break out of the infinite loop
+/// and shut themselves down.
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
 /// The ThreadPool object uses a "channel" to send "jobs" (which are just functions) to
 /// thread pool Workers. The workers get the "jobs" by reading from the "channel" and execute
 /// the them.
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 impl ThreadPool {
@@ -30,26 +39,58 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for _ in &self.workers {
+            self.sender.send(Terminate).unwrap();
+        }
+
+        for worker in &mut self.workers {
+            println!("Waiting for worker {} to shut down", worker.id);
+            // Option.take() takes the value out of the option, leaving a None in its place.
+            // We need to do this because we have a worker ref here but "join()" wants to take
+            // ownership of the handle.
+            if let Some(handle) = worker.thread.take() {
+                handle.join().unwrap();
+            }
+        }
+
+        println!("All workers have been shut down!");
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+    /// Loop until receiving a Terminate message.
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Self {
         let thread = thread::spawn(move || loop {
             // recv() blocks if there's no job yet
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {} got a job; executing.", id);
+            let message = receiver.lock().unwrap().recv().unwrap();
 
-            // Execute the actual task by calling the job function.
-            job();
+            match message {
+                // Execute the actual task by calling the job function.
+                NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job()
+                }
+                Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break;
+                }
+            }
         });
 
-        Self { id, thread }
+        Self {
+            id,
+            thread: Some(thread),
+        }
     }
 }
