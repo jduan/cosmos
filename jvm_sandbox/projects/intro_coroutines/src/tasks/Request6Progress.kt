8d849e1adbe519950e48d0d39cfc5877fe30c9cf
Jingjing Duan <duanjingjing@gmail.com>
2020-08-02 15:05:19 -0700

    show progress

--- jvm_sandbox/projects/intro_coroutines/src/tasks/Request6Progress.kt
+++ jvm_sandbox/projects/intro_coroutines/src/tasks/Request6Progress.kt
@@ -1,11 +1,28 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicInteger
 
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val users = mutableListOf<User>()
+    val count = AtomicInteger()
+    // We iterate through all the repos sequentially here because a new request is sent only
+    // when the previous result is received.
+    return repos.forEach() { repo ->
+        val batch = service.getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        users.addAll(batch)
+        updateResults(users.aggregate(), count.incrementAndGet() == repos.size)
+    }
 }
