3aad7dc3b59fc25ff8e493d7af6656fa972b5ebd
Jingjing Duan <duanjingjing@gmail.com>
2020-08-02 13:59:53 -0700

    use suspend function

--- jvm_sandbox/projects/intro_coroutines/src/tasks/Request4Suspend.kt
+++ jvm_sandbox/projects/intro_coroutines/src/tasks/Request4Suspend.kt
@@ -2,6 +2,19 @@ package tasks
 
 import contributors.*
 
+// This looks surprisingly similar to the "blocking" version. It's readable and expresses
+// exactly what we're trying to achieve.
 suspend fun loadContributorsSuspend(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    // We iterate through all the repos sequentially here because a new request is sent only
+    // when the previous result is received.
+    return repos.flatMap { repo ->
+        service.getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+    }.aggregate()
 }
