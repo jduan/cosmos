b3f0995eeaf8ce31293b5e1b6b78833ad04cc24e
Jingjing Duan <duanjingjing@gmail.com>
2020-08-02 16:29:43 -0700

    coroutines communicate via channels

--- jvm_sandbox/projects/intro_coroutines/src/tasks/Request7Channels.kt
+++ jvm_sandbox/projects/intro_coroutines/src/tasks/Request7Channels.kt
@@ -1,6 +1,7 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -11,6 +12,34 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .bodyList()
+
+        val channel = Channel<List<User>>(repos.size)
+
+        launch {
+            val allUsers = mutableListOf<User>()
+            repeat(repos.size) {
+                val users = channel.receive()
+                allUsers.addAll(users)
+                updateResults(allUsers.aggregate(), it == repos.size - 1)
+            }
+        }
+
+        // We iterate through all the repos and launch a separate coroutine for each repo.
+        // All the coroutines run in parallel.
+        repos.forEach() { repo ->
+            launch {
+                log("start loading for ${repo.name}")
+                // delay here is needed so we have time to cancel the coroutines
+                delay(3000)
+                val users = service.getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                channel.send(users)
+            }
+        }
     }
 }
