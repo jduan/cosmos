f735894885531edb1567091d95e54713f996e04d
Jingjing Duan <duanjingjing@gmail.com>
2020-08-30 22:24:41 -0700

    PMD: check for resource initialization and cleanup in @Before and @After methods

--- jvm_sandbox/projects/kotlin_sandbox/src/test/java/MyJavaTest.java
+++ jvm_sandbox/projects/kotlin_sandbox/src/test/java/MyJavaTest.java
@@ -12,11 +12,12 @@ public class MyJavaTest {
 
   @After
   public void teardown() {
+    // If you comment out this line, you will get a PMD violation. See [EnsureResourceCleanup]
     System.out.println("Current time: " + System.currentTimeMillis());
   }
 
   @Test
   public void hello1() {
-    assertEquals(1, 1);
+    assertEquals("1 should be equal to 1", 1, 1);
   }
 }
--- jvm_sandbox/projects/pmd_rules/src/main/kotlin/jduan/pmd_rules/EnsureResourceCleanup.kt
+++ jvm_sandbox/projects/pmd_rules/src/main/kotlin/jduan/pmd_rules/EnsureResourceCleanup.kt
@@ -1,20 +1,59 @@
 package jduan.pmd_rules
 
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration
-import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression
+import net.sourceforge.pmd.lang.java.ast.ASTName
+import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule
 
+/**
+ * This PMD check looks for a matching pair of "System.currentTimeMillis()" call in
+ * @Before and @After methods for junit tests. This can be generalized to check for
+ * any kind of resource initialization and cleanup.
+ */
 class EnsureResourceCleanup : AbstractJavaRule() {
+    var currentTimeMillisCalled = false
+
     override fun visit(node: ASTMethodDeclaration, data: Any): Any {
-        println("annotations: ${node.declaredAnnotations}")
-
-        val exps = node.findDescendantsOfType(ASTPrimaryExpression::class.java)
-
-        exps.forEach {
-            println("expression: $it")
+        if (isBefore(node)) {
+            currentTimeMillisCalled = isCurrentTimeMillisCalled(node)
+        } else if (isAfter(node)){
+            if (currentTimeMillisCalled) {
+                if (hasViolation(node)) {
+                    addViolationWithMessage(data, node,
+                        "System.currentTimeMillis is called in @Before but not in @After")
+                }
+            }
         }
 
         super.visit(node, data)
         return data
+    }
+
+    private fun isCurrentTimeMillisCalled(node: ASTMethodDeclaration): Boolean {
+        val exps = node.findDescendantsOfType(ASTPrimaryPrefix::class.java)
+
+        return exps.any {
+            val name = it.getFirstDescendantOfType(ASTName::class.java)
+            if (name != null) {
+                name.image == "System.currentTimeMillis"
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun hasViolation(node: ASTMethodDeclaration): Boolean {
+        return !isCurrentTimeMillisCalled(node)
+    }
+
+    private fun isBefore(node: ASTMethodDeclaration) = hasAnnotation(node, "Before")
+
+    private fun isAfter(node: ASTMethodDeclaration) = hasAnnotation(node, "After")
+
+    private fun hasAnnotation(node: ASTMethodDeclaration, annotationName: String): Boolean {
+        return node.declaredAnnotations.any {
+            val name = it.getFirstDescendantOfType(ASTName::class.java)
+            name?.image == annotationName
+        }
     }
 }
