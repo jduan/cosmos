d6e6772e1519a2368e1ea8d3eac25da3d186ab6b
Jingjing Duan <duanjingjing@gmail.com>
2020-08-31 17:00:36 -0700

    WIP

--- rust_sandbox/zero2prod/src/lib.rs
+++ rust_sandbox/zero2prod/src/lib.rs
@@ -11,12 +11,23 @@ async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     // HttpServer handles all transport level concerns.
     let server = HttpServer::new(|| {
         // App is where all the application logic lives: routing, middlewares, request handlers, etc
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
--- rust_sandbox/zero2prod/tests/health_check.rs
+++ rust_sandbox/zero2prod/tests/health_check.rs
@@ -20,3 +20,48 @@ async fn health_check_tests() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
+
+#[actix_rt::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=John%20Wick&email=johnwick%40gmail.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=John%20Wick", "missing the email"),
+        ("email=johnwick%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API didn't fail with 400 Bad Request when the payload was {}",
+            error_message
+        );
+    }
+}
