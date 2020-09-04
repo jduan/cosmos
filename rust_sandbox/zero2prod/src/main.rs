b77d5270a59a2ae567de09ffb42463bfcce923a1
Jingjing Duan <duanjingjing@gmail.com>
2020-09-03 22:16:43 -0700

    add configuration

--- rust_sandbox/zero2prod/src/main.rs
+++ rust_sandbox/zero2prod/src/main.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 // This macro expands into a regular main function that starts an async runtime and
 // drives it to completion.
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to address");
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind to address");
     run(listener)?.await
 }
