4e8bb1ce0d222b72dbbd6e55222efe57f3b7a6a1
Jingjing Duan <duanjingjing@gmail.com>
2021-01-18 11:47:23 -0800

    JsonNode

--- jvm_sandbox/projects/java_sandbox/src/test/java/jackson/JsonNodeTest.java
+++ jvm_sandbox/projects/java_sandbox/src/test/java/jackson/JsonNodeTest.java
@@ -4,16 +4,17 @@ import static org.junit.Assert.assertEquals;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
 import java.io.IOException;
+import java.util.Iterator;
 import org.junit.Test;
 
 /**
- * Jackson has a built-in tree model which can be used to represent a JSON object.
- * Jackson's tree model is useful if you don't know how the JSON you will receive
- * looks, or if you for some reason cannot (or just don't want to) create a class
- * to represent it. The Jackson Tree Model is also useful if you need to manipulate
- * the JSON before using or forwarding it. All of these situations can easily occur
- * in a Data Streaming scenario.
+ * Jackson has a built-in tree model which can be used to represent a JSON object. Jackson's tree
+ * model is useful if you don't know how the JSON you will receive looks, or if you for some reason
+ * cannot (or just don't want to) create a class to represent it. The Jackson Tree Model is also
+ * useful if you need to manipulate the JSON before using or forwarding it. All of these situations
+ * can easily occur in a Data Streaming scenario.
  */
 public class JsonNodeTest {
     @Test
@@ -23,6 +24,8 @@ public class JsonNodeTest {
         // You can also do this:
         // JsonNode jsonNode = objectMapper.readTree(carJson);
         JsonNode jsonNode = objectMapper.readValue(carJson, JsonNode.class);
+
+        assertEquals("Mercedes", jsonNode.get("brand").asText());
     }
 
     @Test
@@ -55,5 +58,37 @@ public class JsonNodeTest {
         JsonNode jsonNode = objectMapper.valueToTree(car);
 
         assertEquals("Cadillac", jsonNode.get("brand").asText());
+    }
+
+    @Test
+    public void testJsonPath() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = "{" +
+            "\"person\": {" +
+            "\"name\": \"James\"," +
+            "\"ssn\": \"abc123\"" +
+            "}}";
+
+        JsonNode node = objectMapper.readTree(jsonString);
+        // JsonNode.at takes a JSON "path expression", which specifies the complete path from the
+        // root JsonNode and down to the field you want to access the value of.
+        assertEquals("James", node.at("/person/name").asText());
+        assertEquals("abc123", node.at("/person/ssn").asText());
+    }
+
+    @Test
+    public void testListOfObjects() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String carsString = "[" +
+            "{ \"brand\" : \"Mercedes\", \"doors\" : 5 }, " +
+            "{ \"brand\" : \"Honda\", \"doors\" : 4 }, " +
+            "{ \"brand\" : \"Toyota\", \"doors\" : 3 }" +
+            "]";
+        JsonNode node = objectMapper.readTree(carsString);
+        for (Iterator<JsonNode> it = node.iterator(); it.hasNext(); ) {
+            JsonNode child = it.next();
+            JsonNode doors = child.get("doors");
+            System.out.println("doors: " + doors.asText());
+        }
     }
 }
