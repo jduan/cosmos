680558ffc9de4a5c947a634eed0b8a73fa226772
Jingjing Duan <duanjingjing@gmail.com>
2021-08-06 19:33:14 -0700

    maven: multi-module projects

---
+++ maven/multiproject/simple-weather/src/main/java/com/jduan/weather/Weather.java
@@ -0,0 +1,22 @@
+package com.jduan.weather;
+
+public class Weather {
+    private String temp;
+    private String humidity;
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+}
