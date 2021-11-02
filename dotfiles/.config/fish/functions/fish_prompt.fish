eb6078907d9b9d69311ff66c5feefd91fb85472e
Jingjing Duan <duanjingjing@gmail.com>
2021-11-02 08:56:30 -0700

    restore fish prompt for git

--- dotfiles/.config/fish/functions/fish_prompt.fish
+++ dotfiles/.config/fish/functions/fish_prompt.fish
@@ -12,56 +12,56 @@ function fish_prompt --description 'Write out the prompt'
     echo -n -s ' ' "$__fish_prompt_cwd" (prompt_pwd) " $__fish_prompt_normal" '> '
 end
 
-# set normal (set_color normal)
-# set magenta (set_color magenta)
-# set yellow (set_color yellow)
-# set green (set_color green)
-# set red (set_color red)
-# set gray (set_color -o black)
-#
-# # Fish git prompt
-# set __fish_git_prompt_showdirtystate 'yes'
-# # set __fish_git_prompt_showstagedstate 'yes'
-# # set __fish_git_prompt_showstashstate 'yes'
-# set __fish_git_prompt_showuntrackedfiles 'yes'
-# set __fish_git_prompt_showupstream 'yes'
-# set __fish_git_prompt_color_branch yellow
-# set __fish_git_prompt_color_upstream_ahead green
-# set __fish_git_prompt_color_upstream_behind red
-#
-# # Status Chars
-# set __fish_git_prompt_char_dirtystate '✘'
-# # set __fish_git_prompt_char_stagedstate '→'
-# # set __fish_git_prompt_char_stashstate '↩'
-# set __fish_git_prompt_char_untrackedfiles '☡'
-# set __fish_git_prompt_char_upstream_ahead ' ⬆ '
-# set __fish_git_prompt_char_upstream_behind ' ⬇ '
-#
-#
-# function fish_prompt
-#   set last_status $status
-#
-#   set_color $fish_color_cwd
-#   printf '%s' (prompt_pwd)
-#   set_color normal
-#
-#   # Don't show "git prompt" for large repos!
-#   set BIG_REPOS treehouse fullhouse airbnb chef data apps
-#   set is_big_repo false
-#   for repo in $BIG_REPOS
-#     if string match --regex --quiet $repo $PWD
-#       set is_big_repo true
-#       break
-#     end
-#   end
-#   if test $is_big_repo = true
-#     set git_repo_root (git rev-parse --show-toplevel)
-#     set git_repo_name (basename $git_repo_root)
-#     # Show repo name for large repos
-#     printf ' (%s) ' $git_repo_name
-#   else
-#     printf '%s ' (__fish_git_prompt)
-#   end
-#
-#   set_color normal
-# end
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+# set __fish_git_prompt_showstagedstate 'yes'
+# set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '✘'
+# set __fish_git_prompt_char_stagedstate '→'
+# set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_upstream_ahead ' ⬆ '
+set __fish_git_prompt_char_upstream_behind ' ⬇ '
+
+
+function fish_prompt
+  set last_status $status
+
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  # Don't show "git prompt" for large repos!
+  set BIG_REPOS treehouse fullhouse airbnb chef data apps
+  set is_big_repo false
+  for repo in $BIG_REPOS
+    if string match --regex --quiet $repo $PWD
+      set is_big_repo true
+      break
+    end
+  end
+  if test $is_big_repo = true
+    set git_repo_root (git rev-parse --show-toplevel)
+    set git_repo_name (basename $git_repo_root)
+    # Show repo name for large repos
+    printf ' (%s) ' $git_repo_name
+  else
+    printf '%s ' (__fish_git_prompt)
+  end
+
+  set_color normal
+end
