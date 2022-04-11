83691427320099361a97a1c41084a4c748af2edf
Jingjing Duan <duanjingjing@gmail.com>
2022-04-10 19:37:09 -0700

    JDBI example

--- jvm_sandbox/projects/java_sandbox/src/main/java/misc/JdbiExample.java
+++ jvm_sandbox/projects/java_sandbox/src/main/java/misc/JdbiExample.java
@@ -1,8 +1,13 @@
 package misc;
 
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.generic.GenericType;
+import org.jdbi.v3.core.result.ResultIterable;
+import org.jdbi.v3.core.result.ResultProducers;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class JdbiExample {
     public static void main(String[] args) {
@@ -12,8 +17,12 @@ public class JdbiExample {
 
             // Named parameters
             handle.createUpdate("INSERT INTO user (id, name) VALUES (:id, :name)")
+                    .bind("id", 1)
+                    .bind("name", "John")
+                    .execute();
+            handle.createUpdate("INSERT INTO user (id, name) VALUES (:id, :name)")
                     .bind("id", 2)
-                    .bind("name", "Clarice")
+                    .bind("name", "Ryan")
                     .execute();
 
             List<String> names = handle.createQuery("SELECT name FROM user ORDER BY name")
@@ -21,9 +30,26 @@ public class JdbiExample {
                     .mapTo(String.class)
                     .list();
 
-            for (String name : names) {
-                System.out.println("name: " + name);
+            List<String> names2 = handle.createQuery("SELECT name FROM user ORDER BY name")
+                    .execute(ResultProducers.returningResults())
+                    .collectInto(new GenericType<List<String>>() {});
+
+            ResultIterable<Map<String, Object>> names3 = handle.createQuery("SELECT id, name FROM user ORDER BY name")
+                    .execute(ResultProducers.returningResults())
+                    .mapToMap();
+            List<Map<String, Object>> names4 = names3.stream().collect(Collectors.toList());
+
+
+            for (Map<String, Object> map : names4) {
+                for (String key : map.keySet()) {
+                    System.out.println(String.format("key: %s, value: %s", key, map.get(key)));
+                }
+                System.out.println();
             }
+
+//            for (String name : names2) {
+//                System.out.println("name: " + name);
+//            }
 
         });
     }
