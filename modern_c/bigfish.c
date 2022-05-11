bba490fa9eb5a2f49a93b76ad37a5450ddc97f5c
Jingjing Duan <duanjingjing@gmail.com>
2022-05-11 16:47:38 -0700

    define constant N

--- modern_c/bigfish.c
+++ modern_c/bigfish.c
@@ -1,6 +1,9 @@
 /* Copyright (c) 2020, Dive into Systems, LLC (https://diveintosystems.org/)  */
 #include <stdio.h>
 #include <stdlib.h>
+
+#define N 10
+
 /* print size elms of array p with name name */
 void print_array(int *p, int size, char *name) ;
 
@@ -8,18 +11,18 @@ int main(int argc, char *argv[]) {
     int *bigfish, *littlefish, i;
 
     // allocate space for two int arrays
-    bigfish = (int *)malloc(sizeof(int)*10);
-    littlefish = (int *)malloc(sizeof(int)*10);
+    bigfish = (int *)malloc(sizeof(int)*N);
+    littlefish = (int *)malloc(sizeof(int)*N);
     if (!bigfish || !littlefish) {
         printf("Error: malloc failed\n");
         exit(1);
     }
-    for (i=0; i < 10; i++) {
-        bigfish[i] = 10+i;
+    for (i=0; i < N; i++) {
+        bigfish[i] = N+i;
         littlefish[i] = i;
     }
-    print_array(bigfish,10, "bigfish");
-    print_array(littlefish,10, "littlefish");
+    print_array(bigfish,N, "bigfish");
+    print_array(littlefish,N, "littlefish");
 
     // here is a bad Heap memory access
     // (write beyond bounds of allocated memory):
@@ -27,8 +30,8 @@ int main(int argc, char *argv[]) {
         bigfish[i] = 66+i;
     }
     printf("\nafter loop:\n");
-    print_array(bigfish,10, "bigfish");
-    print_array(littlefish,10, "littlefish");
+    print_array(bigfish,N, "bigfish");
+    print_array(littlefish,N, "littlefish");
 
     free(bigfish);
     free(littlefish);  // program will crash here
