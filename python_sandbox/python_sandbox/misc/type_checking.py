5f99dc1fa9f5b3241b9a0c0b672c17485b8a5a8b
Jingjing Duan <duanjingjing@gmail.com>
2022-09-05 21:38:09 -0700

    add type annotations

--- python_sandbox/python_sandbox/misc/type_checking.py
+++ python_sandbox/python_sandbox/misc/type_checking.py
@@ -1,10 +1,15 @@
 import random
+from typing import Dict, List, Tuple
+
+# type aliases
+Card = Tuple[str, str]
+Deck = List[Card]
 
 SUITS = "♠ ♡ ♢ ♣".split()
 RANKS = "2 3 4 5 6 7 8 9 10 J Q K A".split()
 
 
-def create_deck(shuffle=False):
+def create_deck(shuffle=False) -> Deck:
     """Create a new deck of 52 cards"""
     deck = [(s, r) for r in RANKS for s in SUITS]
     if shuffle:
@@ -12,16 +17,16 @@ def create_deck(shuffle=False):
     return deck
 
 
-def deal_hands(deck):
+def deal_hands(deck: Deck) -> Tuple[Deck, Deck, Deck, Deck]:
     """Deal the cards in the deck into four hands"""
     return deck[0::4], deck[1::4], deck[2::4], deck[3::4]
 
 
-def play():
+def play() -> None:
     """Play a 4-player card game"""
     deck = create_deck(shuffle=True)
-    names = "P1 P2 P3 P4".split()
-    hands = {n: h for n, h in zip(names, deal_hands(deck))}
+    names: List[str] = "P1 P2 P3 P4".split()
+    hands: Dict[str, Deck] = {n: h for n, h in zip(names, deal_hands(deck))}
 
     for name, cards in hands.items():
         card_str = " ".join(f"{s}{r}" for (s, r) in cards)
