f470392c2373729463ab502bdefb6ce5108edb04
Jingjing Duan <duanjingjing@gmail.com>
2024-06-19 09:38:10 -0700

    handle identifiers and keywords

--- jlox-lang/app/src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ jlox-lang/app/src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -3,9 +3,33 @@ package com.craftinginterpreters.lox;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Scanner {
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and", AND);
+    keywords.put("class", CLASS);
+    keywords.put("else", ELSE);
+    keywords.put("false", FALSE);
+    keywords.put("for", FOR);
+    keywords.put("fun", FUN);
+    keywords.put("if", IF);
+    keywords.put("nil", NIL);
+    keywords.put("or", OR);
+    keywords.put("print", PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super", SUPER);
+    keywords.put("this", THIS);
+    keywords.put("true", TRUE);
+    keywords.put("var", VAR);
+    keywords.put("while", WHILE);
+  }
+
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
   // point to the first character in the lexeme being scanned
@@ -96,6 +120,8 @@ public class Scanner {
       default:
         if (isDigit(c)) {
           number();
+        } else if (isAlpha(c)) {
+          identifier();
         } else {
           Lox.error(line, "Unexpected character.");
         }
@@ -103,8 +129,23 @@ public class Scanner {
     }
   }
 
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+
+    String text = source.substring(start, current);
+    addToken(keywords.getOrDefault(text, IDENTIFIER));
+  }
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+  }
+
   private boolean isDigit(char c) {
     return c >= '0' && c <= '9';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
   }
 
   private void number() {
