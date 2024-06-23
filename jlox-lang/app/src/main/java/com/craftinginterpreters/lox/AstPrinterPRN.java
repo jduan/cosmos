5863e902a624d1b752c5bb15a3bd3b7a8d83b95d
Jingjing Duan <duanjingjing@gmail.com>
2024-06-22 18:27:45 -0700

    more complex expressions

--- jlox-lang/app/src/main/java/com/craftinginterpreters/lox/AstPrinterPRN.java
+++ jlox-lang/app/src/main/java/com/craftinginterpreters/lox/AstPrinterPRN.java
@@ -29,11 +29,13 @@ public class AstPrinterPRN implements Expr.Visitor<String> {
 
   // testing only
   public static void main(String[] args) {
-    Expr expression =
+    Expr binaryExpr =
         new Expr.Binary(
             new Expr.Unary(new Token(TokenType.MINUS, "-", null, 1), new Expr.Literal(123)),
             new Token(TokenType.STAR, "*", null, 1),
             new Expr.Grouping(new Expr.Literal(45.67)));
+    Expr expression =
+        new Expr.Binary(binaryExpr, new Token(TokenType.MINUS, "-", null, 1), binaryExpr);
     System.out.println(expression.accept(new AstPrinterPRN()));
   }
 }
