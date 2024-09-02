873c28b239a57eb4c0d34388ea71fc12bf58d802
Jingjing Duan <duanjingjing@gmail.com>
2024-09-02 14:10:58 -0700

    add more nix tools to nix_tools.sh

--- dotfiles/nix_tools.sh
+++ dotfiles/nix_tools.sh
@@ -1,20 +1,47 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # List of packages
 packages=(
+  actionlint
   asdf-vm
+  bat
+  bazelisk
+  colordiff
   coreutils
   diff-so-fancy
   fasd
   fd
   fish
+  fswatch
+  fzf
+  gh
   git
+  google-cloud-sdk
+  htop
   iterm2
+  jq
+  kubectl
+  kubectx
+  kubernetes-helm
+  ncdu
+  pkgdiff
+  pstree
   python3
   rbenv
+  reattach-to-user-namespace
   ripgrep
+  shellcheck
+  terraform
+  tldr
   tmux
+  tmuxinator
+  tokei
+  tree
   vim
+  wget
+  yamllint
+  yq-go
 )
 
 # Iterate through the array
