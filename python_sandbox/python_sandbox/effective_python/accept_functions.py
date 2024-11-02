a15b2b8f5f9420be8a5970b1693dbc7c4257e9c3
Jingjing Duan <duanjingjing@gmail.com>
2024-11-02 11:15:35 -0700

    effective python

---
+++ python_sandbox/python_sandbox/effective_python/accept_functions.py
@@ -0,0 +1,50 @@
+names = ['Socrates', 'Archimedes', 'Plato', 'Aristotle']
+names.sort(key=len)
+print(names)
+
+def log_missing():
+    print('key added')
+    return 0
+
+from collections import defaultdict
+current = {'green': 12, 'blue': 3}
+increments = [
+    ('red', 5),
+    ('blue', 17),
+    ('orange', 9),
+]
+result = defaultdict(log_missing, current)
+print('Before:', dict(result))
+for key, amount in increments:
+    result[key] += amount
+print('After:', dict(result))
+print('current should not have changed: ', current)
+
+class CountMissing:
+    def __init__(self):
+        self.added = 0
+
+    def missing(self):
+        self.added += 1
+        return 0
+
+counter = CountMissing()
+result = defaultdict(counter.missing, current)
+for key, amount in increments:
+    result[key] += amount
+assert counter.added == 2
+
+
+class BetterCountMissing:
+    def __init__(self):
+        self.added = 0
+
+    def __call__(self, *args, **kwargs):
+        self.added += 1
+        return 0
+
+counter = BetterCountMissing()
+result = defaultdict(counter, current)
+for key, amount in increments:
+    result[key] += amount
+assert counter.added == 2
---
+++ python_sandbox/python_sandbox/effective_python/use_classmethod.py
@@ -0,0 +1,146 @@
+import os
+import random
+import tempfile
+from threading import Thread
+
+
+class InputData:
+    def read(self):
+        raise NotImplementedError
+
+
+class PathInputData(InputData):
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+
+    def read(self):
+        with open(self.path) as fd:
+            return fd.read()
+
+
+class Worker:
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.result = None
+
+    def map(self):
+        raise NotImplementedError
+
+    def reduce(self, other):
+        raise NotImplementedError
+
+
+class LineCountWorker(Worker):
+    def map(self):
+        data = self.input_data.read()
+        self.result = data.count('\n')
+
+    def reduce(self, other):
+        self.result += other.result
+
+
+def generate_inputs(data_dir):
+    for name in os.listdir(data_dir):
+        yield PathInputData(os.path.join(data_dir, name))
+
+
+def create_workers(input_list):
+    workers = []
+    for input_data in input_list:
+        workers.append(LineCountWorker(input_data))
+    return workers
+
+
+def execute(workers):
+    threads = [Thread(target=w.map) for w in workers]
+    for thread in threads: thread.start()
+    for thread in threads: thread.join()
+
+    first, *rest = workers
+    for worker in rest:
+        first.reduce(worker)
+    return first.result
+
+
+def mapreduce(data_dir):
+    inputs = generate_inputs(data_dir)
+    workers = create_workers(inputs)
+    return execute(workers)
+
+
+def write_test_files(tmpdir):
+    for i in range(100):
+        with open(os.path.join(tmpdir, str(i)), 'w') as fd:
+            fd.write('\n' * random.randint(0, 100))
+
+
+with tempfile.TemporaryDirectory() as tmpdir:
+    write_test_files(tmpdir)
+    result = mapreduce(tmpdir)
+    print(f"There are {result} lines")
+
+
+class GenericInputData:
+    def read(self):
+        raise NotImplementedError
+
+    @classmethod
+    def generate_inputs(cls, config):
+        raise NotImplementedError
+
+
+class PathInputData2(GenericInputData):
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+
+    def read(self):
+        with open(self.path) as fd:
+            return fd.read()
+
+    @classmethod
+    def generate_inputs(cls, config):
+        data_dir = config['data_dir']
+        for name in os.listdir(data_dir):
+            yield cls(os.path.join(data_dir, name))
+
+
+class GenericWorker:
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.result = None
+
+    def map(self):
+        raise NotImplementedError
+
+    def reduce(self, other):
+        raise NotImplementedError
+
+    @classmethod
+    def create_workers(cls, input_class, config):
+        workers = []
+        for input_data in input_class.generate_inputs(config):
+            workers.append(cls(input_data))
+        return workers
+
+
+class LineCountWorker2(GenericWorker):
+    def map(self):
+        data = self.input_data.read()
+        self.result = data.count('\n')
+
+    def reduce(self, other):
+        self.result += other.result
+
+
+def mapreduce2(worker_class, input_class, config):
+    workers = worker_class.create_workers(input_class, config)
+    return execute(workers)
+
+
+with tempfile.TemporaryDirectory() as tmpdir:
+    write_test_files(tmpdir)
+    config = {'data_dir': tmpdir}
+    result = mapreduce2(LineCountWorker2, PathInputData2, config)
+    print(f"There are {result} lines")
