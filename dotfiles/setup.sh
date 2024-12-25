caf79697f94ad12de151e96f5cdb4a773340918a
Jingjing Duan <duanjingjing@gmail.com>
2024-12-25 07:56:28 -0800

    add config for espanso

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -4,3 +4,6 @@ set -euxo pipefail
 # Link to ~/github/jduan/notes
 ln -s ~/github/jduan/notes ~/notes
 sudo ln -s $HOME/.nix-profile/bin/reattach-to-user-namespace /usr/local/bin/hmmm
+
+# espanso
+ln -s ~/github/jduan/cosmos/dotfiles/espanso "$HOME/Library/Application Support/espanso"
