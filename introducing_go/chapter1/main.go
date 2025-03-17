7d6a0d5892941b2a94a07cab4bc092255510180c
Jingjing Duan <duanjingjing@gmail.com>
2025-03-16 21:51:43 -0700

    introducing go: chapter 1

---
+++ introducing_go/chapter1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, world!")
+	// os.Exit(1)
+	fmt.Println("You won't see this line")
+	fmt.Println("Hello, my name is Jingjing")
+}
