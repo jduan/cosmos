a7bbbc3e11cd5b49f53a3679ef975b36dfae3079
Jingjing Duan <duanjingjing@gmail.com>
2025-03-31 07:06:50 -0700

    add list_open_prs.sh

---
+++ misc_scripts/list_open_prs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# This script shows all of your open PRs.
+
+GITHUB_ORG=bay1inc
+
+gh search prs --author @me --state open --owner "$GITHUB_ORG" --json title,url,isDraft,repository | \
+jq -r 'map(select(.isDraft == false)) | sort_by(.updatedAt) | reverse[] | "\(.repository.name): \(.title)\n\(.url)\nLast updated: \(.updatedAt)\n"'
