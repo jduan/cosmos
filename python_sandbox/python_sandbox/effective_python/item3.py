02ef8bc7c45b8188b4ff1f7305816d3abcac5537
Jingjing Duan <duanjingjing@gmail.com>
2025-07-06 10:47:52 -0700

    add item3.py

---
+++ python_sandbox/python_sandbox/effective_python/item3.py
@@ -0,0 +1,22 @@
+def bad_reference():
+    # this is valid python code even if my_var isn't declared
+    print(my_var)
+    my_var = 123
+
+
+def sometimes_ok(x):
+    if x:
+        my_var = 123
+    print(my_var)
+
+
+def bad_math():
+    # this is valid python code too
+    return 1 / 0
+
+
+if __name__ == "__main__":
+    bad_math()
+    sometimes_ok(True)
+    sometimes_ok(False)
+    bad_reference()
