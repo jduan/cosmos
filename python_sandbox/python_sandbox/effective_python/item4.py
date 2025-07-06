36f16fc05ed19933eb985f1c2ceaf55e9271759a
Jingjing Duan <duanjingjing@gmail.com>
2025-07-06 11:18:52 -0700

    update item4.py

--- python_sandbox/python_sandbox/effective_python/item4.py
+++ python_sandbox/python_sandbox/effective_python/item4.py
@@ -1,26 +1,22 @@
 from urllib.parse import parse_qs
 
 
+def query_params():
+    my_values = parse_qs("red=5&blue=0&green=", keep_blank_values=True)
+    print(repr(my_values))
+    print("Red: ", get_first_int(my_values, "red"))
+    print("Green: ", get_first_int(my_values, "green"))
+    print("Opacity: ", get_first_int(my_values, "opacity"))
+
+
+# move complex expressions into helper functions, esp. if you need to use
+# the same logic repeatedly.
 def get_first_int(values, key, default=0):
     found = values.get(key, [""])
     if found[0]:
-        found = int(found[0])
-    else:
-        found = default
-    return found
-
-
-def main():
-    my_values = parse_qs("red=5&blue=0&green=", keep_blank_values=True)
-    print("my_values: %s" % my_values)
-    red = get_first_int(my_values, "red")
-    green = get_first_int(my_values, "green")
-    opacity = get_first_int(my_values, "opacity")
-
-    print("Red: %r" % red)
-    print("Green: %r" % green)
-    print("Opacity: %r" % opacity)
+        return int(found[0])
+    return default
 
 
 if __name__ == "__main__":
-    main()
+    query_params()
