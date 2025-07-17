509a9aadfc1c036f5f7bc1072e652e5181811d42
Jingjing Duan <duanjingjing@gmail.com>
2025-07-17 07:26:05 -0700

    item14.py

--- python_sandbox/python_sandbox/effective_python/item14.py
+++ python_sandbox/python_sandbox/effective_python/item14.py
@@ -1,17 +1,56 @@
-# Functions that return None to indicate special meaning are error prone because None and other
-# values (e.g., zero, the empty string) all evaluate to False in conditional expressions.
-# You can return a tuple (success, result) and the caller can check if success is True
-def divide(a, b):
-    try:
-        return True, a / b
-    except ZeroDivisionError:
-        return False, None
+"""
+Know how to slice sequences
+"""
 
+# You can slice built-in types like: list, tuple, str, bytes
 
-# Raise exceptions to indicate special situations instead of returning None. Expect the calling
-# code to handle exceptions properly when they’re documented.
-def divide2(a, b):
-    try:
-        return a / b
-    except ZeroDivisionError as e:
-        raise ValueError("Invalid inputs") from e
+a = ["a", "b", "c", "d", "e", "f", "g", "h"]
+assert a[3:5] == ["d", "e"]
+assert a[1:7] == ["b", "c", "d", "e", "f", "g"]
+
+# slice from the start
+assert a[:5] == a[0:5]
+
+# slice from the end
+assert a[5:] == a[5 : len(a)]
+
+print(a[:])  # ["a", "b", "c", "d", "e", "f", "g", "h"]
+print(a[:5])  # ["a", "b", "c", "d", "e"]
+print(a[:-1])  # ["a", "b", "c", "d", "e", "f", "g"]
+print(a[4:])  # ["e", "f", "g", "h"]
+print(a[-3:])  # ["f", "g", "h"]
+print(a[2:5])  # ["c", "d", "e"]
+print(a[2:-1])  # ["c", "d", "e", "f", "g"]
+print(a[-3:-1])  # ["f", "g"]
+
+# slicing deals properly with start and end indices that are beyond the boundaries
+# of a list by silently omitting missing items.
+assert a[5:] == a[5:20]
+assert a[:5] == a[-20:5]
+
+# but directly accessing the same missing index causes an exception
+try:
+    print(a[20])
+    raise RuntimeError("Expected an exception but it didn't happen")
+except IndexError:
+    pass
+
+list_of_lists = [[1, 2], [3, 4]]
+list_of_lists2 = list_of_lists[1:]
+list_of_lists2[0].append(5)
+# The result of slicing is a whole new list. However, each of the elements in the new
+# list will refer to the corresponding objects from the original list. Modifying the
+# existing objects change the original list too!
+assert list_of_lists == [[1, 2], [3, 4, 5]]
+
+list_of_lists2.append([5, 6])
+assert list_of_lists2 == [[3, 4, 5], [5, 6]]
+# appending a new item to list_of_lists doesn't change list_of_lists
+assert list_of_lists == [[1, 2], [3, 4, 5]]
+
+# slicing can be used in assignments
+a = ["a", "b", "c", "d", "e", "f", "g", "h"]
+print(a)
+a[2:7] = ["H", "E"]
+print(a)
+assert a == ["a", "b", "H", "E", "h"]
