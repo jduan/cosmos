e90f8932c05dea9acd71d967ca61a82be8bd68da
Jingjing Duan <duanjingjing@gmail.com>
2025-07-21 07:13:48 -0700

    item18.py

--- python_sandbox/python_sandbox/effective_python/item18.py
+++ python_sandbox/python_sandbox/effective_python/item18.py
@@ -1,67 +1,57 @@
-# # Things to Remember
-#
-# Functions can accept a variable number of positional arguments by using *args in the def
-# statement.
-#
-# You can use the items from a sequence as the positional arguments for a function with the *
-# operator.
-#
-# Using the * operator with a generator may cause your program to run out of memory and crash.
-#
-# Adding new positional parameters to functions that accept *args can introduce hard-to-find
-# bugs.
+"""
+Use "zip" to process iterators in parallel.
+"""
+
+from itertools import zip_longest
+from textwrap import fill
+
+names = ["Cecilia", "Lise", "Marie"]
+counts = [len(n) for n in names]
+print(counts)
 
 
-def log(message, *values):
-    # The type of values is a tuple.
-    # print(type(values))
-    if values:
-        values_str = ", ".join(str(x) for x in values)
-        print("%s: %s" % (message, values_str))
-    else:
-        print(message)
+def find_longest_name(names):
+    counts = [len(n) for n in names]
+    longest_name = None
+    max_count = 0
+    for i in range(len(names)):
+        count = counts[i]
+        if count > max_count:
+            longest_name = names[i]
+            max_count = count
+    return longest_name
 
 
-# One problem with *args is that you can’t add new positional arguments to your function in the
-# future without migrating every caller. If you try to add a positional argument in the front of the
-# argument list, existing callers will subtly break if they aren’t updated.
-#
-# To avoid this possibility entirely, you should use keyword-only arguments when you want to extend
-# functions that accept *args
-def log2(sequence, message, *values):
-    if values:
-        values_str = ", ".join(str(x) for x in values)
-        print("%s: %s: %s" % (sequence, message, values_str))
-    else:
-        print("%s: %s" % (sequence, message))
+print(f"Longest name: {find_longest_name(names)}")
 
 
-# arguments after varargs are keyword-only arguments!
-def log3(message, *values, sequence):
-    if values:
-        values_str = ", ".join(str(x) for x in values)
-        print("%s: %s: %s" % (sequence, message, values_str))
-    else:
-        print("%s: %s" % (sequence, message))
+def find_longest_name2(names):
+    counts = [len(n) for n in names]
+    longest_name = None
+    max_count = 0
+    for name, count in zip(names, counts):
+        if count > max_count:
+            longest_name = name
+            max_count = count
+    return longest_name
 
 
-def main():
-    log("My numbers are", 1, 2)
-    log("Hi there")
-    favorites = [7, 33, 99]
-    # This is called the * or splat operator which unpacks a list and passes them
-    # to the log function as var args.
-    log("Favorite colors", *favorites)
+print(f"Longest name: {find_longest_name2(names)}")
 
-    # this works
-    log2(1, "Favorites", 7, 33)
-    # old usage breaks silently because the sequence will be 'Favorite numbers'
-    log2("Favorite numbers", 7, 33)
+names.append("Rosalind")
+# this doesn't show "Rosalind" because zip keeps yielding tuples until any one of the
+# wrapped iteators is exhausted.
+for name, count in zip(names, counts):
+    print(f"name: {name}, count: {count}")
 
-    # leverage keyword-only arguments
-    log3("Favorites", 7, 33, sequence=1)
-    log3("Favorite numbers", 7, 33, sequence=2)
+# You can pass "strict=True" to make zip raise an exception if any of the inputs is
+# exhausted before the others
+try:
+    for name, count in zip(names, counts, strict=True):
+        print(f"name: {name}, count: {count}")
+except ValueError:
+    pass
 
-
-if __name__ == "__main__":
-    main()
+# zip_longest fills in missing values for shorter iterators using the "fillvalue"
+for name, count in zip_longest(names, counts, fillvalue=0):
+    print(f"name: {name}, count: {count}")
