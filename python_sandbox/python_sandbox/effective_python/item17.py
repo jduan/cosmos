00a453d66a11e175f750d7c8844d6e44ebfbee64
Jingjing Duan <duanjingjing@gmail.com>
2025-07-21 07:01:55 -0700

    item17.py

--- python_sandbox/python_sandbox/effective_python/item17.py
+++ python_sandbox/python_sandbox/effective_python/item17.py
@@ -1,54 +1,30 @@
-def normalize(numbers):
-    """
-    This function doesn't work for generators because the 2nd loop of the generator wouldn't
-    do anything because it has been exhausted by the first loop!
-    """
-    total = sum(numbers)
-    return [100 * value / total for value in numbers]
+"""
+Prefer enumerate over range
+"""
 
+import enum
+from random import randint
 
-def normalize_copy(numbers):
-    """
-    This function works for generators by converting the generator to a list first.
-    """
-    numbers = list(numbers)
-    total = sum(numbers)
-    return [100 * value / total for value in numbers]
+random_bits = 0
+for i in range(32):
+    if randint(0, 1):
+        random_bits |= 1 << i
 
+print(bin(random_bits))
 
-def normalize_func(get_iter):
-    """
-    get_iter is a function that returns an iterator so it doesn't have the problem of generators
-    getting exhausted.
-    """
-    total = sum(get_iter())
-    return [100 * value / total for value in get_iter()]
+flavor_list = ["vanilla", "chocolate", "pecan", "strawberry"]
+for flavor in flavor_list:
+    print(f"{flavor} is delicious")
 
+# this works but isn't elegant
+for i in range(len(flavor_list)):
+    flavor = flavor_list[i]
+    print(f"{i + 1}: {flavor}")
 
-# The iterator protocol is how Python for loops and related expressions traverse the contents of a
-# container type. When Python sees a statement like for x in foo it will actually call iter(foo).
-# The iter built-in function calls the foo.__iter__ special method in turn. The __iter__ method must
-# return an iterator object (which itself implements the __next__ special method). Then the for loop
-# repeatedly calls the next built-in function on the iterator object until it’s exhausted (and
-# raises a StopIteration exception).
-#
-# It sounds complicated, but practically speaking you can achieve all of this behavior for your
-# classes by implementing the __iter__ method as a generator.
+# enumerator yields pairs of the loop index and the next value from the given iterator
+for i, flavor in enumerate(flavor_list):
+    print(f"{i + 1}: {flavor}")
 
-
-class ReadVisit(object):
-    """
-    A class that implements the "iterator protocol". You can use it in a for loop. The important
-    thing is you can iterate it many times. The iterators returned are advanced and exhausted
-    independently. For example, you can use it in the "normalize" function.
-
-    The only downsize of this approach is that it reads the input data multiple times!
-    """
-
-    def __init__(self, data_path):
-        self.data_path = data_path
-
-    def __iter__(self):
-        with open(self.data_path) as fd:
-            for line in fd:
-                yield int(line)
+# begin counting at 1 instead of 0
+for i, flavor in enumerate(flavor_list, 1):
+    print(f"{i}: {flavor}")
