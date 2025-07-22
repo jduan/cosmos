817f7fc697c928af787b32199fe2c5da22d27aac
Jingjing Duan <duanjingjing@gmail.com>
2025-07-22 08:04:33 -0700

    item21.py

--- python_sandbox/python_sandbox/effective_python/item21.py
+++ python_sandbox/python_sandbox/effective_python/item21.py
@@ -1,64 +1,85 @@
-# Things to Remember
-#
-# Keyword arguments make the intention of a function call more clear.
-#
-# Use keyword-only arguments to force callers to supply keyword arguments for potentially
-# confusing functions, especially those that accept multiple Boolean flags.
-#
-# Python 3 supports explicit syntax for keyword-only arguments in functions.
-#
-# Python 2 can emulate keyword-only arguments for functions by using **kwargs and manually
-# raising TypeError exceptions.
+"""
+Be defensive when iterating over arguments.
+
+Beware of functions and methods that iterate over input arguments multiple
+times. If these arguments are iterators, you might see strange behavior and
+missing values.
+"""
 
 
-def safe_division(number, divisor, ignore_overflow=False, ignore_zero_division=False):
-    try:
-        return number / divisor
-    except OverflowError:
-        if ignore_overflow:
-            return 0
-        else:
-            raise
-    except ZeroDivisionError:
-        if ignore_zero_division:
-            return float("inf")
-        else:
-            raise
+def normalize(numbers):
+    total = sum(numbers)
+    percentages = []
+    for val in numbers:
+        percentages.append(100 * val / total)
+    return percentages
 
 
-# In python 3, you can define keyword-only arguments. These arguments can only be
-# supplied by keyword, never by position.
-# The * symbol indicates the end of positional arguments and the beginning of
-# keyword-only arguments.
-def safe_division2(
-    number, divisor, *, ignore_overflow=False, ignore_zero_division=False
-):
-    try:
-        return number / divisor
-    except OverflowError:
-        if ignore_overflow:
-            return 0
-        else:
-            raise
-    except ZeroDivisionError:
-        if ignore_zero_division:
-            return float("inf")
-        else:
-            raise
+visits = [15, 35, 80]
+percentages = normalize(visits)
+print(percentages)
 
 
-def main():
-    print("ignore overflow: %s" % safe_division(10**500, 1, ignore_overflow=True))
-    print("ignore overflow: %s" % safe_division(10**500, 1, True))
-    print("ignore overflow: %s" % safe_division(10**500, 1, True, False))
-    print("ignore zero division: %s" % safe_division(1, 0, ignore_zero_division=True))
-
-    # these won't work
-    # print("ignore overflow: %s" % safe_division2(10**500, 1, True))
-    # print("ignore overflow: %s" % safe_division2(10**500, 1, True, False))
-    print("ignore overflow: %s" % safe_division2(10**500, 1, ignore_overflow=True))
-    print("ignore zero division: %s" % safe_division2(1, 0, ignore_zero_division=True))
+def read_visits(data_path):
+    with open(data_path) as f:
+        for line in f:
+            yield int(line)
 
 
-if __name__ == "__main__":
-    main()
+it = read_visits("my_numbers.txt")
+percentages2 = normalize(it)
+print(percentages2)
+
+
+def normalize_copy(numbers):
+    """same as normalize but it copies the argument so it can be used twice"""
+    numbers_copy = list(numbers)
+    total = sum(numbers_copy)
+    percentages = []
+    for val in numbers_copy:
+        percentages.append(100 * val / total)
+    return percentages
+
+
+it = read_visits("my_numbers.txt")
+percentages3 = normalize_copy(it)
+print(percentages3)
+
+
+def normalize_func(get_iter):
+    """This addresses a problem with normalize_copy: copying the iterator could cause
+    the program to run out of memory and it defeats the purpose of writing "read_visits"
+    as a generator in the first place.
+
+    This function takes a lambda instead and we can call it each time."""
+    total = sum(get_iter())
+    percentages = []
+    for val in get_iter():
+        percentages.append(100 * val / total)
+    return percentages
+
+
+percentages4 = normalize_func(lambda: read_visits("my_numbers.txt"))
+print(percentages4)
+
+
+class ReadVisits:
+    def __init__(self, data_path) -> None:
+        self.data_path = data_path
+
+    def __iter__(self):
+        with open(self.data_path) as f:
+            for line in f:
+                yield (int(line))
+
+
+visits = ReadVisits("my_numbers.txt")
+"""
+This works because the sum method in normalize calls ReadVisits.__iter__ to
+allocate a new iterator object. The for loop to normalize the numbers also calls
+__iter__ to allocate a second iterator object. Each of those iterators will be
+advanced and exhausted independently, ensuring that each unique iteration sees
+all of the input data values. The only downside of this approach is that it
+reads the input data multiple times.  """
+percentages5 = normalize(visits)
+print(percentages5)
