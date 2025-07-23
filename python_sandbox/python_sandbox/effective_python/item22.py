8c5b613fe2488e1eced6d87d0ec4d911834730fd
Jingjing Duan <duanjingjing@gmail.com>
2025-07-23 08:57:29 -0700

    item22.py

--- python_sandbox/python_sandbox/effective_python/item22.py
+++ python_sandbox/python_sandbox/effective_python/item22.py
@@ -1,94 +1,83 @@
-# Things to Remember
-#
-# Avoid making dictionaries with values that are other dictionaries or long tuples.
-#
-# Use namedtuple for lightweight, immutable data containers before you need the flexibility of a
-# full class.
-#
-# Move your bookkeeping code to use multiple helper classes when your internal state dictionaries
-# get complicated.
+"""
+Never modify containers while iterating over them; use copies or caches instead.
+"""
 
-from collections import defaultdict, namedtuple
+# if you add a new item to a dict while iterating over it, python will raise a runtime exception
+search_key = "red"
+my_dict = {"red": 1, "blue": 2, "green": 3}
+# for key in my_dict:
+#     if key == "blue":
+#         my_dict["yellow"] = 4
 
+# same thing happens if you delete an item while iterating over it
+# for key in my_dict:
+#     if key == "blue":
+#         del my_dict["green"]
 
-class SimpleGradebook(object):
-    def __init__(self):
-        self._grades = {}
+# but it's ok to update an item while iterating it
+for key in my_dict:
+    if key == "blue":
+        my_dict["green"] = 4
+print(my_dict)
 
-    def add_student(self, name):
-        self._grades[name] = []
+# set is slightly different; if you add an item that already exists in the set, it won't
+# cause any problems while iterating over it
+my_set = {"red", "blue", "green"}
+for color in my_set:
+    if color == "blue":
+        my_set.add("green")
 
-    def report_grade(self, name, score):
-        self._grades[name].append(score)
+# list is similar to dict; if update an existing index, it's ok
+my_list = [1, 2, 3]
+for number in my_list:
+    print(number)
+    if number == 2:
+        my_list[0] = -1
+print(f"my_list: {my_list}")
 
-    def average_grade(self, name):
-        grades = self._grades[name]
-        return sum(grades) / len(grades)
+# if you try to insert an item before the current iterator position, your code will
+# get stuck in an infinite loop
+my_list = [1, 2, 3]
+for number in my_list:
+    print(number)
+    # if number == 2:
+    #     my_list.insert(0, 4)
 
+# but appending to the list or inserting an item after the current iterator position is ok!
+my_list = [1, 2, 3]
+for number in my_list:
+    print(number)
+    if number == 2:
+        # both of these are fine
+        # my_list.append(4)
+        my_list.insert(2, 5)
+print(f"my_list: {my_list}")
 
-class BySubjectGradebook(object):
-    """
-    self._grades is a map that maps student name to another map, which maps subject to grade.
-    """
+"""
+Looking at each of the examples above, it can be hard to guess whether the code
+will work in all cases. Modifying containers during iteration can be especially
+error prone in situations where the modification point changes based on input to
+the algorithm. In some cases it'll work, and in others there will be an error.
+Thus, my advice is to never modify containers while you iterate over them.
+"""
 
-    def __init__(self):
-        self._grades = {}
+# if you really want to make modifications during iteration due to the nature
+# of your algorithm, you can make a copy of the container and iterate the copy.
+# the same works well for lists and sets!
+my_dict = {"red": 1, "blue": 2, "green": 3}
+keys_copy = list(my_dict.keys())
+for key in keys_copy:
+    if key == "blue":
+        my_dict["green"] = 4
+print(f"my_dict: {my_dict}")
 
-    def add_student(self, name):
-        self._grades[name] = defaultdict(list)
-
-    def report_grade(self, name, subject, score):
-        self._grades[name][subject].append(score)
-
-    def average_grade(self, name):
-        by_subject = self._grades[name]
-        total, count = 0, 0
-        for grades in by_subject.values():
-            total += sum(grades)
-            count += len(grades)
-        return total / count
-
-
-Grade = namedtuple("Grade", ("score", "weight"))
-
-
-class Subject(object):
-    def __init__(self):
-        self._grades = []
-
-    def report_grade(self, score, weight):
-        self._grades.append(Grade(score, weight))
-
-    def average_grade(self):
-        total, total_weight = 0, 0
-        for grade in self._grades:
-            total += grade.score * grade.weight
-            total_weight += grade.weight
-        return total / total_weight
-
-
-class Student(object):
-    def __init__(self):
-        self._subjects = {}
-
-    def subject(self, name):
-        if name not in self._subjects:
-            self._subjects[name] = Subject()
-        return self._subjects[name]
-
-    def average_grade(self):
-        total, count = 0, 0
-        for subject in self._subjects.values():
-            total += subject.average_grade()
-            count += 1
-        return total / count
-
-
-class WeightedGradebook(object):
-    def __init__(self):
-        self._students = {}
-
-    def student(self, name):
-        if name not in self._students:
-            self._students[name] = Student()
-        return self._students[name]
+# for some extremely large containers, copying might be too slow. One way to deal
+# with poor performance is to stage modifications in a separate container and merge
+# the changes back to the main data structure after iteration.
+my_dict = {"red": 1, "blue": 2, "green": 3}
+modifications = {}
+for key in my_dict:
+    if key == "blue":
+        modifications["green"] = 4
+my_dict.update(modifications)
+print(f"my_dict: {my_dict}")
